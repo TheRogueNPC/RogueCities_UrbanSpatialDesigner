eb8e353278d8e23602ef709764f4bc36a7264a21
TheRogueNPC <Team.Crow@yahoo.com>
2026-02-05 02:58:37 -0800

    Remove duplicate generator files from core/src/ (should be in generators/src/)

--- core/src/Generators/Tensors/BasisFields.cpp
+++
@@ -1,8 +0,0 @@
-#include "RogueCity/Generators/Tensors/BasisFields.hpp"
-
-namespace RogueCity::Generators {
-
-    // BasisFields.cpp is header-only - all implementations are inline
-    // This file exists for CMake consistency and future expansion
-
-} // namespace RogueCity::Generators
